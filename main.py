ed3008d4e9568aa5fc34eb55e0e72266b183fd64
Bob Sheridan <djmousefarm@gmail.com>
2025-04-11 19:06:04 +0100

    This is the state of the code at the start of the project.  At timestamp 3:31:17 in the freecodecamp tutorial

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 
 suits = ['spades', 'clubs', 'hearts', 'diamonds']
 
-suit = suits[2]
-rank = 'K'
-value = 10
-
-print(f"Your card is : {rank} of {suit}")
-print(f"Your card value is : {value}")  
-
-suits.append('snakes')
-
-
 for suit in suits:
     print(suit)
